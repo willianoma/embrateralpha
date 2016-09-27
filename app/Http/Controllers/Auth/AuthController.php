7d4beba84594381d9778a629b666f652f62fd37d
Willian Acioly <Willian Acioly>
2016-09-27 14:36:04 -0300

    auth modify

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -6,6 +6,7 @@ use Illuminate\Contracts\Auth\Registrar;
 use Illuminate\Foundation\Auth\AuthenticatesAndRegistersUsers;
 
 class AuthController extends Controller {
+	protected $redirectPath = 'motivos';
 
 	/*
 	|--------------------------------------------------------------------------
--- app/Http/routes.php
+++ app/Http/routes.php
@@ -15,13 +15,25 @@
 
 Route::get('/', 'HomeController@index');
 
+Route::group(['prefix' => '', 'middleware' => 'auth'], function(){
 
 Route::resource("funcionarios","FuncionarioController");
 Route::resource("postos","PostoController"); 
 Route::resource("motivos","MotivoController");
 Route::resource("ocorrencias","OcorrenciaController"); 
+});
 
 Route::controllers([
 	'auth' => 'Auth\AuthController',
 	'password' => 'Auth\PasswordController',
 ]);
+
+
+// Authentication routes...
+Route::get('auth/login', 'Auth\AuthController@getLogin');
+Route::post('auth/login', 'Auth\AuthController@postLogin');
+Route::get('auth/logout', 'Auth\AuthController@getLogout');
+
+// Registration routes...
+Route::get('auth/register', 'Auth\AuthController@getRegister');
+Route::post('auth/register', 'Auth\AuthController@postRegister');
