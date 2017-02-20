d687af5cb43d0bd831ca178eb81739bda0265c8f
willianoma <willianoma@hotmail.com>
2017-02-20 14:23:57 -0300

    Atualizações, inicio modulo calculoFaltas de importação do software SHPonto

---
+++ app/Http/Middleware/GetFile.php
@@ -0,0 +1,34 @@
+<?php
+
+class getfile
+{
+
+    protected function ola()
+    {
+
+        $calculofaltas = new CalculoFaltas();
+
+
+        $arquivo = fopen('teste.txt', 'r');
+
+        while (!feof($arquivo)) {
+
+            $linha = fgets($arquivo);
+            $piece = explode("|", $linha);
+
+            $calculofaltas::$referencia = $piece[0];
+            $calculofaltas::$codigoevento = $piece[1];
+            $calculofaltas::$faltas = $piece[2];
+            $calculofaltas::$dataInicio = $piece[3];
+            $calculofaltas::$dataFinal = $piece[4];
+
+        }
+
+
+        fclose($arquivo);
+
+        return $calculofaltas;
+
+    }
+
+}
---
+++ resources/views/calculofaltas/index.blade.php
@@ -0,0 +1,1 @@
+<H1>Aqui index calculo faltas</H1>
