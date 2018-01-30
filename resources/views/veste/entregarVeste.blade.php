39a3500c850c82452fe49c20b2d0a808e1ab9a44
willianoma <willianoma@hotmail.com>
2018-01-30 18:20:43 -0300

    EntregaVeste

--- resources/views/veste/entregarVeste.blade.php
+++ resources/views/veste/entregarVeste.blade.php
@@ -3,38 +3,44 @@
 @section('content')
     <form action="#">
 
-        <div class="mdl-grid">
+        <input type="hidden" value="{{auth()->user()->id}}" name="idusuario">
 
-            <div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label mdl-cell mdl-cell--12-col">
-                <input class="mdl-textfield__input" type="text" id="sample3">
-                <label class="mdl-textfield__label" for="sample3">Text...</label>
-            </div>
+        <div class="mdl-grid">
 
 
             <div class="mdl-textfield mdl-js-textfield getmdl-select mdl-cell mdl-cell--12-col">
-                <input type="text" value="" class="mdl-textfield__input" id="sample2" readonly>
-                <input type="hidden" value="" name="sample2">
+                <input type="text" value="" class="mdl-textfield__input" id="funcionario" readonly>
+                <input type="hidden" value="funcionario" name="funcionario">
                 <i class="mdl-icon-toggle__label material-icons">keyboard_arrow_down</i>
-                <label for="sample2" class="mdl-textfield__label">Country</label>
-                <ul for="sample2" class="mdl-menu mdl-menu--bottom-left mdl-js-menu">
-                    <li class="mdl-menu__item" data-val="DEU">Germany</li>
-                    <li class="mdl-menu__item" data-val="BLR">Belarus</li>
-                    <li class="mdl-menu__item" data-val="RUS">Russia</li>
+                <label for="funcionario" class="mdl-textfield__label">Funcionário</label>
+                <ul for="funcionario" class="mdl-menu mdl-menu--bottom-left mdl-js-menu">
+                    <li class="mdl-menu__item" data-val="DEU">Funcionario</li>
                 </ul>
+            </div>
 
+            <div class="mdl-textfield mdl-js-textfield getmdl-select mdl-cell mdl-cell--12-col">
+                <input type="text" value="" class="mdl-textfield__input" id="veste" readonly>
+                <input type="hidden" value="veste" name="veste">
+                <i class="mdl-icon-toggle__label material-icons">keyboard_arrow_down</i>
+                <label for="veste" class="mdl-textfield__label">Veste</label>
+                <ul for="veste" class="mdl-menu mdl-menu--bottom-left mdl-js-menu">
+                    <li class="mdl-menu__item" data-val="DEU">Veste</li>
+                </ul>
             </div>
 
 
-            <div class="mdl-cell mdl-cell--12-col">2 (4 phone)</div>
+            <div class="mdl-textfield mdl-js-textfield mdl-cell mdl-cell--12-col">
+                <textarea class="mdl-textfield__input" type="text" rows="3" id="obs"></textarea>
+                <label class="mdl-textfield__label" for="obs">Observações</label>
+            </div>
+
+            <div class="mdl-cell mdl-cell--12-col">
+                assinatura
+            </div>
+
 
         </div>
 
 
     </form>
 @endsection
-
-funcionario
-veste
-usuario
-asssinatura
-obs
