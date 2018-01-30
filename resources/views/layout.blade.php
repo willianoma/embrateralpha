9ad94d730d3545322d29f041315642965a01b766
willianoma <willianoma@hotmail.com>
2018-01-30 18:09:15 -0300

    Instal MDL+ MDL Select

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -41,6 +41,16 @@
     <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
     <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
     <![endif]-->
+
+    {{--Matarial Design Lite--}}
+    <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+    <link rel="stylesheet" href="https://code.getmdl.io/1.3.0/material.indigo-pink.min.css">
+    <script defer src="https://code.getmdl.io/1.3.0/material.min.js"></script>
+
+
+    <link rel="stylesheet" href="/componentes/getmdl-select/getmdl-select.min.css">
+    <script defer src="/componentes/getmdl-select/getmdl-select.min.js"></script>
+
 </head>
 
 <body>
@@ -161,7 +171,6 @@
 <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
 <!-- <script src="../../assets/js/ie10-viewport-bug-workaround.js"></script> -->
 <script src="https://cdnjs.cloudflare.com/ajax/libs/bootbox.js/4.4.0/bootbox.min.js"></script>
-
 
 
 </body>
--- resources/views/veste/entregarVeste.blade.php
+++ resources/views/veste/entregarVeste.blade.php
@@ -1,5 +1,40 @@
 @extends('layout')
 
 @section('content')
+    <form action="#">
 
+        <div class="mdl-grid">
+
+            <div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label mdl-cell mdl-cell--12-col">
+                <input class="mdl-textfield__input" type="text" id="sample3">
+                <label class="mdl-textfield__label" for="sample3">Text...</label>
+            </div>
+
+
+            <div class="mdl-textfield mdl-js-textfield getmdl-select mdl-cell mdl-cell--12-col">
+                <input type="text" value="" class="mdl-textfield__input" id="sample2" readonly>
+                <input type="hidden" value="" name="sample2">
+                <i class="mdl-icon-toggle__label material-icons">keyboard_arrow_down</i>
+                <label for="sample2" class="mdl-textfield__label">Country</label>
+                <ul for="sample2" class="mdl-menu mdl-menu--bottom-left mdl-js-menu">
+                    <li class="mdl-menu__item" data-val="DEU">Germany</li>
+                    <li class="mdl-menu__item" data-val="BLR">Belarus</li>
+                    <li class="mdl-menu__item" data-val="RUS">Russia</li>
+                </ul>
+
+            </div>
+
+
+            <div class="mdl-cell mdl-cell--12-col">2 (4 phone)</div>
+
+        </div>
+
+
+    </form>
 @endsection
+
+funcionario
+veste
+usuario
+asssinatura
+obs
